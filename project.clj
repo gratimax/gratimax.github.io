b4cf082b53b8feb165fac2be4ba30089bcae290e
Max Ovsiankin <max@ovsankin.com>
2016-08-07 14:21:51 -0700

    Switch to leiningen project structure

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject code "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.34"]
-                 [cljsjs/d3 "3.5.7-1"]]
+                 [cljsjs/d3 "3.5.7-1"]
+                 [stasis "2.3.0"]]
   :plugins [[lein-cljsbuild "1.1.3"]]
+  :source-path ["src/clj/" "src/cljc/"]
   :cljsbuild {:builds
               {:college-decisions-tracker
-               {:source-paths ["src"]
+               {:source-paths ["src/cljs/" "src/cljc"]
                 :compiler     {:output-to     "../_site/js/colleges.js"
                                :main          "blog.colleges.core"
                                :asset-path    "/js"
